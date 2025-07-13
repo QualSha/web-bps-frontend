d54c85085b86c2766312c9072736fd49b10aaf18
QualSha <haykalpashasrg@gmail.com>
2025-07-13 11:48:55 +0700

    pub2

--- src/components/PublicationListPage.jsx
+++ src/components/PublicationListPage.jsx
@@ -133,7 +133,7 @@ export default function PublicationListPage() {
       </div>
 
       {/* Modal Konfirmasi Delete */}
-      {showConfirmModal && (
+      {/* {showConfirmModal && (
         <div className="fixed inset-0 bg-black/80 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg p-6 max-w-md w-full mx-4">
             <h3 className="text-lg font-semibold text-gray-800 mb-4">
@@ -168,7 +168,7 @@ export default function PublicationListPage() {
             </div>
           </div>
         </div>
-      )}
+      )} */}
 
     </div>
   );
