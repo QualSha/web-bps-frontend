7e0bde11131eacb46023ee98c946490e14d25bf5
QualSha <haykalpashasrg@gmail.com>
2025-07-13 10:40:54 +0700

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+// src/components/Footer.jsx
+import React from "react";
+export default function Footer() {
+  return (
+    <footer className="text-center py-8">
+      <p className="text-gray-500 text-sm">
+        Created by Haykal Pasha Siregar (222313117@stis.ac.id)
+      </p>
+    </footer>
+  );
+}
